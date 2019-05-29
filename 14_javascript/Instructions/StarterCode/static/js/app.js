c44075e6d0da95415f0f501e1dcf432d547cbb3a
Alexander Lecocq <alexander.m.lecocq@gmail.com>
2019-05-29 11:36:10 -0500

    working filters with dropdown

--- 14_javascript/Instructions/StarterCode/static/js/app.js
+++ 14_javascript/Instructions/StarterCode/static/js/app.js
@@ -1,38 +1,16 @@
 // from data.js
 var tableData = data;
 
-// Select table body
-var table_body = d3.select('#table_body');
+var table_body = d3.select('#table-body');
 
-// Select the submit button
+var filters = d3.select('#filters')
+
 var filter_button = d3.select("#filter-btn");
 
-// Complete the click handler for the form
-filter_button.on("click", function() {
+// Display default filter (datetime)
+changeFilter();
 
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select('#datetime');
-
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-
-  // Clear table body of any existing rows
-  table_body.selectAll("tr").remove();
-
-  // Use the form input to filter the data by blood type
-  tableData.filter(sighting => sighting['datetime'] == inputValue).forEach(sighting => {
-      var row = table_body.append('tr');
-      Object.values(sighting).forEach(data_entry => {
-        var cell = row.append('td');
-        cell.text(data_entry)
-      })
-  })
-});
-
-
+// Populate the initial table
 tableData.forEach(sighting => {
     var row = table_body.append('tr');
     Object.values(sighting).forEach(data_entry => {
@@ -40,3 +18,41 @@ tableData.forEach(sighting => {
       cell.text(data_entry)
     })
 })
+
+
+
+// Events
+
+// Filter table and display
+filter_button.on("click", function() {
+    d3.event.preventDefault();
+    var inputElement = d3.select('#' + filter_type);
+    var inputValue = inputElement.property("value");
+    // Clear output table
+    table_body.selectAll("tr").remove();
+    // Take entire table if no filter applied, otherwise only collect matching rows
+    if (inputValue == '') {
+        var outputData = tableData;
+    }
+    else {
+        var outputData = tableData.filter(sighting => sighting[filter_type] == inputValue);
+    }
+    // Add the filtered data to the table body
+    outputData.forEach(sighting => {
+        var row = table_body.append('tr');
+        Object.values(sighting).forEach(data_entry => {
+          var cell = row.append('td');
+          cell.text(data_entry)
+        })
+    })
+  });
+
+// Change which filter is showing
+function changeFilter() {
+    // Figure out which filter has been chosen
+    filter_type = d3.select('#select-filter').node().value;
+    // Hide whatever existing filter exists
+    filters.selectAll('li').style('display', 'none');
+    // Display chosen filter
+    d3.select('#' + filter_type + '-filter').style('display', 'block');
+}
